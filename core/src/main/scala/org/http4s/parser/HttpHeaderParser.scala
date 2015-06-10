fc621bad5de6c4b6d4265b682285fecb23dc894f
Bryce Anderson <bryce.anderson22@gmail.com>
2015-06-09 21:37:59 -0400

    Add methods to allow modification of the parser registry.
    I also renamed the class to HttpHeaderParser. That resulted in the bulk of the changes in the commit log. The only significant changes are in HttpHeaderParser.scala.

---
+++ core/src/main/scala/org/http4s/parser/HttpHeaderParser.scala
@@ -0,0 +1,138 @@
+/*
+ * Derived from https://github.com/spray/spray/blob/v1.1-M7/spray-http/src/main/scala/spray/http/parser/HttpParser.scala
+ *
+ * Copyright (C) 2011-2012 spray.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.http4s
+package parser
+
+import java.util
+
+import org.http4s.util.CaseInsensitiveString
+
+import Header.Parsed
+import org.http4s.util.string._
+
+
+object HttpHeaderParser extends HttpHeaderParser
+
+private[parser] trait HttpHeaderParser extends SimpleHeaders
+                    with AcceptHeader
+                    with AcceptLanguageHeader
+                    with CacheControlHeader
+                    with ContentTypeHeader
+                    with CookieHeader
+                    with AcceptCharsetHeader
+                    with AcceptEncodingHeader
+                    with AuthorizationHeader
+                    with RangeParser
+                    with LocationHeader
+                    with ProxyAuthenticateHeader
+                    with WwwAuthenticateHeader {
+
+  type HeaderParser = String => ParseResult[Parsed]
+
+  // the java hashmap is measurably faster than the scala collections
+  // Scala 2.11 has an AnyRefMap that is faster.
+  private type Storage = util.HashMap[CaseInsensitiveString, HeaderParser]
+
+  private var allParsers = new Storage()
+
+
+  // Constructor
+  gatherBuiltIn()
+
+  /** Add a parser to the global header parser registry
+    *
+    * Note that modification of the parser collection is not thread safe and care
+    * should be take to ensure all parsers are registered at startup in a single threaded
+    * manner.
+    * 
+    * @param key name of the header to register the parser for
+    * @param parser [[Header]] parser
+    * @return any existing parser already registered to that key
+    */
+  def addParser(key: CaseInsensitiveString, parser: HeaderParser): Option[HeaderParser] = {
+    // elaborate clone etc instead of a `put` to avoid potential `ConcurrentModificationException`s.
+    val next = allParsers.clone().asInstanceOf[Storage]
+    val r = Option(next.put(key, parser))
+    allParsers = next
+    r
+  }
+
+
+  /** Remove the parser for the secified header key
+    *
+    * Note that modification of the parser collection is not thread safe and care
+    * should be take to ensure all parsers are registered at startup in a single threaded
+    * manner.
+    *
+    * @param key name of the header to be removed
+    * @return `Some(parser)` if the parser exists, else `None`
+    */
+  def dropParser(key: CaseInsensitiveString): Option[HeaderParser] = {
+    val next = allParsers.clone().asInstanceOf[Storage]
+    val r = Option(next.remove(key))
+    allParsers = next
+    r
+  }
+
+
+  def parseHeader(header: Header.Raw): ParseResult[Header] = {
+    allParsers.get(header.name) match {
+      case null =>  ParseResult.success(header) // if we don't have a rule for the header we leave it unparsed
+      case parser => parser(header.value)
+    }
+  }
+
+  /**
+   * Warm up the header parsers by triggering the loading of most classes in this package,
+   * so as to increase the speed of the first usage.
+   */
+  def warmUp() {
+    val results = List(
+      Header("Accept", "*/*,text/plain,custom/custom"),
+      Header("Accept-Charset", "*,UTF-8"),
+      Header("Accept-Encoding", "gzip,custom"),
+      Header("Accept-Language", "*,nl-be,custom"),
+      Header("Authorization", "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="),
+      Header("Cache-Control", "no-cache"),
+      Header("Connection", "close"),
+      Header("Content-Disposition", "form-data"),
+      Header("Content-Encoding", "deflate"),
+      Header("Content-Length", "42"),
+      Header("Content-Type", "application/json"),
+      Header("Cookie", "http4s=cool"),
+      Header("Host", "http4s.org"),
+      Header("X-Forwarded-For", "1.2.3.4"),
+      Header("Fancy-Custom-Header", "yeah")
+    ) map parseHeader
+    assert(results.forall(_.isRight))
+  }
+
+  private def gatherBuiltIn(): Unit =
+    this
+      .getClass
+      .getMethods
+      .filter(_.getName.forall(!_.isLower)) // only the header rules have no lower-case letter in their name
+      .foreach { method =>
+      val key = method.getName.replace('_', '-').ci
+      val parser ={ value: String =>
+        method.invoke(this, value)
+      }.asInstanceOf[HeaderParser]
+
+      addParser(key, parser)
+    }
+}
